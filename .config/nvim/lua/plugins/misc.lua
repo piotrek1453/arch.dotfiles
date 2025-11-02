cd91753d7f98ba7a12f48ebdd9d1e026608d13d7
piotrek1453 <83782776+piotrek1453@users.noreply.github.com>
2025-11-02 09:51:19 +0100

    always open kitty fullscreen, add remote editing over ssh to nvim

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -14,4 +14,17 @@ return {
 			return opts
 		end,
 	},
+	-- remote dev
+	{
+		"nosduco/remote-sshfs.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+		opts = {
+			-- Refer to the configuration section below
+			-- or leave empty for defaults
+		},
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		lazy = true,
+	},
 }
