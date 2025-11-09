ffc882ade46e2a573d95276afd75d87ee10290af
piotrek1453 <83782776+piotrek1453@users.noreply.github.com>
2025-11-09 21:52:56 +0100

    python formatting tweaks

--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -1,11 +1,30 @@
 return {
 	{
+		"mason.nvim",
+		opts = {
+			ensure_installed = {
+				"isort",
+				"ruff",
+				"black",
+			},
+		},
+	},
+	{
 		"linux-cultist/venv-selector.nvim",
 		opts = {
 			search = {
 				my_venvs = {
 					command = "fd 'python$' ~/.venv",
 				},
+			},
+		},
+	},
+	-- order imports, format with ruff and black as fallback
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				python = { "isort", "black", "ruff_format" },
 			},
 		},
 	},
