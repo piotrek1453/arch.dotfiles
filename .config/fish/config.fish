08ceecd49bc22cc6eaff572779c1819c065ce703
piotrek1453 <83782776+piotrek1453@users.noreply.github.com>
2025-11-09 12:51:50 +0100

    basic setup script+litex workaround+fish theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,10 +9,18 @@ set PATH $PATH /home/$USER/.local/bin
 # User paths
 fish_add_path /opt/gowin-eda-ide/bin
 
+# User venv - workaround for global litex install
+if test -f $HOME/.venv/bin/activate.fish
+    . "$HOME/.venv/bin/activate.fish"
+end
+
+# Aliases
+alias yay=paru
+alias pacman=sudo pacman
+
 # pnpm
 set -gx PNPM_HOME "/home/$USER/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
-
---
+++ install_deps.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+# install system packages
+grep -v "^#" arch_packages.txt | paru -S --needed --noconfirm -
+
+# install fisher and tide theme
+curl -sL https://git.io/fisher | fish -c "source; fisher install jorgebucaran/fisher IlanCosman/tide@v6"
+# tide configure # run by hand to set up the theme
