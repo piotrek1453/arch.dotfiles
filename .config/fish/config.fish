acb852c6ca0c8c75e8631044a94f2c4b8e1996c7
piotrek1453 <83782776+piotrek1453@users.noreply.github.com>
2025-11-11 12:56:16 +0100

    automatically add latest version of Xilinx tools to path in config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,14 +3,21 @@ if status is-interactive
 end
 set fish_greeting
 
-# Created by `pipx` on 2025-03-21 19:39:15
-set PATH $PATH /home/$USER/.local/bin
-
 # User paths
-# for IDE installed from AUR, doesn't work for now so import from manually installed one is needed as below
-# fish_add_path /opt/gowin-eda-ide/bin 
+## for IDE installed from AUR, doesn't work for now so import from manually installed one is needed as below
+## fish_add_path /opt/gowin-eda-ide/bin 
 fish_add_path $HOME/Tools/Gowin/IDE/bin
+## my scripts
 fish_add_path $HOME/.scripts
+## Xilinx tools
+### Xilinx base path
+set xilinx_root $HOME/Tools/Xilinx
+### Choose path to latest version
+set latest_version (ls $xilinx_root | grep -E '^[0-9]+\.[0-9]+' | sort -V | tail -n 1)
+fish_add_path $xilinx_root/$latest_version/Vitis/bin
+fish_add_path $xilinx_root/$latest_version/Vivado/bin
+# Created by `pipx` on 2025-03-21 19:39:15
+fish_add_path PATH $PATH /home/$USER/.local/bin
 
 # Aliases
 alias yay=paru
