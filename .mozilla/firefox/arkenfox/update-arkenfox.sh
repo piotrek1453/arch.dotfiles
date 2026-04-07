a51daf02dcbe499eda6a91386b5656317c70b3aa
piotrek1453 <83782776+piotrek1453@users.noreply.github.com>
2026-04-07 22:56:15 +0200

    initial usable arkenfox setup + actually working update script

--- .mozilla/firefox/arkenfox/update-arkenfox.sh
+++ .mozilla/firefox/arkenfox/update-arkenfox.sh
@@ -1,6 +1,19 @@
+#!/usr/bin/env bash
+# IMPORTANT: run in actual FF profile dir, not in repo
+
+# update repo
 cd arkenfox-upstream || exit
 git pull origin master
 echo "Updating arkenfox repo"
 cd .. || exit
-cp arkenfox-upstream/user.js user.js
+
+# copy scripts
+# rsync because cp doesn't handle repo symlink well
+rsync -avL arkenfox-upstream/updater.sh .
+rsync -avL arkenfox-upstream/prefsCleaner.sh .
+rsync -avL arkenfox-upstream/user.js .
+
+# run scripts
+./updater.sh
+./prefsCleaner.sh
 echo "Arkenfox updated"
--- .mozilla/firefox/arkenfox/user-overrides.js
+++ .mozilla/firefox/arkenfox/user-overrides.js
@@ -13,5 +13,47 @@ user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", false); // 2
 // user_pref("privacy.clearHistory.historyFormDataAndDownloads", false); // 2830 FF128-135
 // user_pref("privacy.clearHistory.browsingHistoryAndDownloads", false); // 2831 FF136+
 
-// enable smooth scroll (example)
-user_pref("general.smoothScroll", true);
+// don't close browser with last tab closed
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+
+//disable AI stuff
+user_pref("browser.assistant.enabled", false);
+user_pref(
+  "browser.newtabpage.activity-stream.asrouter.providers.messagingassistant",
+  "",
+);
+user_pref(
+  "browser.newtabpage.activity-stream.asrouter.providers.messagingassistant-activity-stream",
+  "",
+);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+user_pref("extensions.activeAddons", "{}");
+user_pref("browser.ml.chat.enabled", false);
+user_pref("browser.ml.chat.page", false);
+user_pref("browser.ml.chat.page.menuBadge", false);
+user_pref("browser.ml.chat.page.footerBadge", false);
+user_pref("browser.ml.chat.menu", false);
+user_pref("browser.ml.chat.shortcuts", false);
+user_pref("browser.ml.chat.sidebar", false);
+user_pref("browser.ml.chat.enabled", false);
+user_pref("browser.ml.linkPreview.enabled", false);
+user_pref("extensions.ml.enabled", false);
+user_pref("sidebar.revamp", false);
+user_pref("sidebar.revamp.defaultLauncherVisible", false);
+user_pref("sidebar.notification.badge.aichat", false);
+
+// enable tab grouping
+user_pref("browser.tabs.groups.enabled", true);
+
+// accept only necessary cookies
+user_pref("network.cookie.cookieBehavior", 1);
+user_pref("network.cookie.lifetimePolicy", 0);
+
+// disable speech dispatcher
+user_pref("media.webspeech.synth.enabled", false);
+
+// prefer dark mode everywhere
+user_pref("ui.systemUsesDarkTheme", 0);
+user_pref("layout.css.prefers-color-scheme.content-override", "dark");
